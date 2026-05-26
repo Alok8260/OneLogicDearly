aa83a2623e892abd9e2fca767f3668706caa31bc
Alok <alokbharati001@gmail.com>
2026-05-26 23:28:16 +0530

    Chnage the package name

--- src/com/java/ArrayProgram/SumOfElementInArray.java
+++ src/com/java/ArrayProgram/SumOfElementInArray.java
@@ -2,9 +2,14 @@ package com.java.ArrayProgram;
 
 public class SumOfElementInArray {
     public static void sumOfElement(){
-
+        int[] arr={4, 5, 3, 9, 2, 1};
+        int sum=0;
+        for (int i = 0; i <=arr.length-1; i++) {
+            sum +=arr[i];
+        }
+        System.out.println(sum);
     }
     public static void main(String[] args) {
-
+        sumOfElement();
     }
 }
