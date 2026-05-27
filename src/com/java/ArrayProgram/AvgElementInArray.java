eed07c57f03c9fc8493696a225065ba74af047bc
Alok <alokbharati001@gmail.com>
2026-05-28 00:32:11 +0530

    Day four of the OneLogicDaily

--- src/com/java/ArrayProgram/AvgElementInArray.java
+++ src/com/java/ArrayProgram/AvgElementInArray.java
@@ -2,9 +2,17 @@ package com.java.ArrayProgram;
 
 public class AvgElementInArray {
     public static void avgElement(){
-
+        int[] arr={3, 7, 8, 9, 5,1, 2};
+        int countArrayElement=0;
+        int totalArrayElement=0;
+        for (int i = 0; i <=arr.length-1; i++) {
+            totalArrayElement+=arr[i];
+            countArrayElement++;
+        }
+        int avgElement= totalArrayElement/countArrayElement;
+        System.out.println("The average of the element is "+ avgElement);
     }
     public static void main(String[] args) {
-
+        avgElement();
     }
 }
