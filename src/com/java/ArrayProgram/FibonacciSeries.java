32c9ec9d9216ae530ab867092dbada48ad11e56e
Alok <alokbharati001@gmail.com>
2026-05-28 22:48:38 +0530

    Day five of the OneLogicDaily

---
+++ src/com/java/ArrayProgram/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package com.java.ArrayProgram;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void fibonacciSeries(int num){
+        int a=0;
+        int b=1;
+        System.out.print("The fibonacci series of "+num + " is ");
+        System.out.print(a +" , ");
+        System.out.print(b+" , ");
+        for(int i=2; i<num; i++){
+            int c=a+b;
+            System.out.print(+ c + " , ");
+            a=b;
+            b=c;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter any Number: ");
+        int number= sc.nextInt();
+        fibonacciSeries(number);
+
+    }
+}
