ebe27fe80821e070465c885f0d294bb341559f34
Alok <alokbharati001@gmail.com>
2026-05-31 17:30:58 +0530

    Add detailed comments to array programs

--- src/com/java/BasicPrograms/CheckNumberIsPrime.java
+++ src/com/java/BasicPrograms/CheckNumberIsPrime.java
@@ -3,28 +3,52 @@ package com.java.BasicPrograms;
 import java.util.Scanner;
 
 public class CheckNumberIsPrime {
-    public static void checkPrime(int a){
-            if(a <= 1) {
-                System.out.println(a + " not a prime number");
-                return;
+
+    // Method to check whether a number is prime or not
+    public static void checkPrime(int a) {
+
+        // Numbers less than or equal to 1 are not prime
+        if (a <= 1) {
+            System.out.println(a + " is not a prime number");
+            return;
+        }
+
+        // Assume the number is prime initially
+        boolean isPrime = true;
+
+        // Check divisibility from 2 to a-1
+        for (int i = 2; i < a; i++) {
+
+            // If divisible by any number, it is not prime
+            if (a % i == 0) {
+                isPrime = false;
+                break;
             }
-            boolean isPrime=true;
-            for (int i=2; i<a; i++){
-                if (a % i == 0){
-                    isPrime=false;
-                    break;
-                }
-            }
-            if (isPrime){
-                System.out.println(a + " is a prime number");
-            }else{
-                System.out.println(a + " is not a prime number");
-            }
+        }
+
+        // Display the result
+        if (isPrime) {
+            System.out.println(a + " is a prime number");
+        } else {
+            System.out.println(a + " is not a prime number");
+        }
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+
+        // Create Scanner object to take user input
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter a number
         System.out.print("Enter any number: ");
-        int a=sc.nextInt();
+
+        // Read the input number
+        int a = sc.nextInt();
+
+        // Call method to check prime
         checkPrime(a);
+
+        // Close Scanner to prevent resource leak
+        sc.close();
     }
 }
