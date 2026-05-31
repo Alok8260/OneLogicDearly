fd8a354ef51336c07bf3539765d3cb5730544153
Alok <alokbharati001@gmail.com>
2026-05-31 17:30:34 +0530

    Add detailed comments to array programs

--- src/com/java/BasicPrograms/SquareOfNumber.java
+++ src/com/java/BasicPrograms/SquareOfNumber.java
@@ -3,15 +3,32 @@ package com.java.BasicPrograms;
 import java.util.Scanner;
 
 public class SquareOfNumber {
-    public static void squareOfNumber(int num){
-        int squareNumber=num*num;
-        System.out.println("Square Number of "+num+ " is " +squareNumber);
+
+    // Method to calculate and display the square of a number
+    public static void squareOfNumber(int num) {
+
+        // Calculate square by multiplying the number by itself
+        int squareNumber = num * num;
+
+        // Display the result
+        System.out.println("Square Number of " + num + " is " + squareNumber);
     }
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter any Number:- ");
-        int number= sc.nextInt();
+
+        // Create Scanner object to take user input
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter any Number: ");
+
+        // Read the number from the user
+        int number = sc.nextInt();
+
+        // Call the method to calculate and print the square
         squareOfNumber(number);
+
+        // Close Scanner to prevent resource leak
+        sc.close();
     }
 }
