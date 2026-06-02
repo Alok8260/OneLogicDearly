720972f7154acacda6fd6a45ae36e1ae558f7d7b
Alok <alokbharati001@gmail.com>
2026-06-02 23:57:24 +0530

    Add detailed comments to array programs

--- src/com/java/StringPrograms/ReverseString.java
+++ src/com/java/StringPrograms/ReverseString.java
@@ -1,9 +1,16 @@
 package com.java.StringPrograms;
+
+import java.util.Scanner;
 public class ReverseString {
-    public static void main(String[] args) {
-        String name = "Alok";
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
         for (int i = name.length() - 1; i >= 0; i--) {
             System.out.print(name.charAt(i));
         }
+    }
+    public static void main(String[] args) {
+        main();
     }
 }
