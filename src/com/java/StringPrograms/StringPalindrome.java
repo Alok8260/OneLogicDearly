bf11de5b308eba7209a3723dfc15e5293b63b419
Alok <alokbharati001@gmail.com>
2026-06-03 23:32:03 +0530

    Add detailed comments to array programs

--- src/com/java/StringPrograms/StringPalindrome.java
+++ src/com/java/StringPrograms/StringPalindrome.java
@@ -3,21 +3,58 @@ package com.java.StringPrograms;
 import java.util.Scanner;
 
 public class StringPalindrome {
-    public static void checkPalindrome(){
-        Scanner sc=new Scanner(System.in);
+
+    // Method to check whether a string is palindrome or not
+    public static void checkPalindrome() {
+
+        // Create Scanner object to take input from user
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user to enter a string
         System.out.print("Enter Your Name: ");
+
+        // Read complete line input from user
         String name = sc.nextLine();
-        String revName="";
-        for (int i = name.length()-1; i >=0; i--) {
-            revName=revName+name.charAt(i);
+
+        // Variable to store reversed string
+        String revName = "";
+
+        /*
+         * Loop starts from last character of string
+         * and moves toward first character
+         */
+        for (int i = name.length() - 1; i >= 0; i--) {
+
+            /*
+             * charAt(i) extracts one character at index i
+             * Then character is added into revName
+             */
+            revName = revName + name.charAt(i);
         }
-        if (name.equalsIgnoreCase(revName)){
+
+        /*
+         * equalsIgnoreCase() compares two strings
+         * ignoring uppercase and lowercase differences
+         */
+        if (name.equalsIgnoreCase(revName)) {
+
+            // If original string and reversed string are same
             System.out.println(name + " is a palindrome String");
-        }else {
-            System.out.println(name+" is not a palindrome string");
+
+        } else {
+
+            // If strings are different
+            System.out.println(name + " is not a palindrome string");
         }
+
+        // Close scanner to prevent resource leak
+        sc.close();
     }
+
+    // Main method - program execution starts from here
     public static void main(String[] args) {
+
+        // Calling palindrome checking method
         checkPalindrome();
     }
 }
