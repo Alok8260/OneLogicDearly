f68682cc22623a271e0f425f8813d95aa82ff0ec
Alok <alokbharati001@gmail.com>
2026-06-03 23:34:29 +0530

    Add detailed comments to array programs

--- src/com/java/StringPrograms/CountTheChar.java
+++ src/com/java/StringPrograms/CountTheChar.java
@@ -1,13 +1,36 @@
 package com.java.StringPrograms;
 
 public class CountTheChar {
+
     public static void main(String[] args) {
-        String name="Alok";
-        int count=0;
-        for (int i = 0; i < name.length() ; i++) {
-            //System.out.println(name.charAt(i));
+
+        // String variable storing the text
+        String name = "Alok";
+
+        // Variable to store character count
+        int count = 0;
+
+        /*
+         * Loop starts from index 0
+         * and runs until the last character
+         *
+         * name.length() gives total number of characters
+         */
+        for (int i = 0; i < name.length(); i++) {
+
+            /*
+             * charAt(i) extracts character at current index
+             * (Currently commented because it is not needed)
+             */
+            // System.out.println(name.charAt(i));
+
+            /*
+             * Increase count by 1 for each character
+             */
             count++;
         }
+
+        // Print total number of characters
         System.out.println(count);
     }
 }
--- src/com/java/StringPrograms/ReverseString.java
+++ src/com/java/StringPrograms/ReverseString.java
@@ -1,17 +1,59 @@
 package com.java.StringPrograms;
 
 import java.util.Scanner;
+
 public class ReverseString {
-    public static void main(){
-        Scanner sc=new Scanner(System.in);
+
+    /*
+     * Custom main method without arguments
+     * This method contains logic to reverse a string
+     */
+    public static void main() {
+
+        // Create Scanner object to take input from user
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user to enter a string
         System.out.print("Enter Your Name: ");
+
+        // Read complete line input from user
         String name = sc.nextLine();
-        String reverse=" ";
+
+        // Variable declared to store reversed string
+        // (Currently not used in this program)
+        String reverse = " ";
+
+        /*
+         * Loop starts from the last index of string
+         * name.length() - 1 gives last character index
+         *
+         * Example:
+         * String = "JAVA"
+         * Indexes = 0 1 2 3
+         *
+         * Loop will run:
+         * 3 -> 2 -> 1 -> 0
+         */
         for (int i = name.length() - 1; i >= 0; i--) {
+
+            /*
+             * charAt(i) extracts character at current index
+             * Characters are printed one by one in reverse order
+             */
             System.out.print(name.charAt(i));
         }
+
+        // Close scanner to prevent resource leak
+        sc.close();
     }
+
+    /*
+     * Actual JVM main method
+     * Program execution starts from here
+     */
     public static void main(String[] args) {
+
+        // Calling custom main() method
         main();
     }
 }
