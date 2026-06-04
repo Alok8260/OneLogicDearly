f31a20b20e3cb13fb07ead7f9d4cf4be22de5a17
Alok <alokbharati001@gmail.com>
2026-06-04 22:42:04 +0530

    Add detailed comments to array programs

--- src/com/java/StringPrograms/CountVowels.java
+++ src/com/java/StringPrograms/CountVowels.java
@@ -2,16 +2,36 @@ package com.java.StringPrograms;
 
 public class CountVowels {
     public static void main(String[] args) {
-        String[] vowel={"A", "E", "I", "O", "U"};
-        String name="sun";
-        int vowelCount=0;
-        for (int i = 0; i <= vowel.length-1; i++) {
+
+        // Array containing all vowels
+        String[] vowel = {"A", "E", "I", "O", "U"};
+
+        // Input string
+        String name = "Alok";
+
+        // Variable to store total vowel count
+        int vowelCount = 0;
+
+        // Print length of the string
+        System.out.println(name.length());
+
+        // Loop through each vowel in the vowel array
+        for (int i = 0; i < vowel.length - 1; i++) {
+
+            // Loop through each character of the string
             for (int j = 0; j < name.length(); j++) {
-                if (vowel[i].equalsIgnoreCase(String.valueOf(name.charAt(j)))){
+
+                // Compare current vowel with current character
+                // equalsIgnoreCase() ignores uppercase/lowercase differences
+                if (vowel[i].equalsIgnoreCase(String.valueOf(name.charAt(j)))) {
+
+                    // Increase vowel count if matched
                     vowelCount++;
                 }
             }
         }
+
+        // Print total number of vowels found
         System.out.println(vowelCount);
     }
 }
