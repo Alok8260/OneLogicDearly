bb25301fc1a104582a96ebc2105e6891aecd3e5a
Alok <alokbharati001@gmail.com>
2026-06-04 22:03:48 +0530

    Day ten of the OneLogicDaily

--- src/com/java/StringPrograms/CountVowels.java
+++ src/com/java/StringPrograms/CountVowels.java
@@ -1,5 +1,17 @@
 package com.java.StringPrograms;
 
 public class CountVowels {
-
+    public static void main(String[] args) {
+        String[] vowel={"A", "E", "I", "O", "U"};
+        String name="sun";
+        int vowelCount=0;
+        for (int i = 0; i <= vowel.length-1; i++) {
+            for (int j = 0; j < name.length(); j++) {
+                if (vowel[i].equalsIgnoreCase(String.valueOf(name.charAt(j)))){
+                    vowelCount++;
+                }
+            }
+        }
+        System.out.println(vowelCount);
+    }
 }
